155038679430c4c5385fdb1eab983241a98bbee8
MC Guenette <mc.c.guenette@gmail.com>
2023-11-20 15:53:40 -0600

    fixed js

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,33 +6,53 @@ const alarmBtn = select('#alarm-btn');
 const clearBtn = select('#clear-btn');
 let alarmTime = null;
 let alarmTimeout = null;
-const audio = new Audio('/path/to/sparkle_your_name.mp3');
+const audio = new Audio('./assets/audio/sparkle_your_name.mp3');
 audio.type = 'audio/mp3';
 audio.loop = true;
 const hours = select('#hours');
 const minutes = select('#minutes');
+const displayTime = select('#time');
+const currentTime = new Date();
+const getHour = formatTime(currentTime.getHours());
+const getMins = formatTime(currentTime.getMinutes());
+
 
 function getCurrentTime() {
-    const displayTime = select('#time');
     const currentTime = new Date();
-    const getHour = formatTime(currentTime.getHours());
-    const getMins = formatTime(currentTime.getMinutes());
-    displayTime.innerText = `${getHour}:${getMins}`;
+    const currentHour = formatTime(currentTime.getHours());
+    const currentMins = formatTime(currentTime.getMinutes());
+    
+    displayTime.innerText = `${currentHour}:${currentMins}`;
 }
 
 function formatTime(time) {
     return time < 10 ? '0' + time : time;
 }
 
-function setAlarm() {
+function validateAlarm() {
+    let numbersRegex = /^[0-9]+$/;
+
+    // Parse input values here
     const hoursInput = parseInt(hours.value, 10);
     const minutesInput = parseInt(minutes.value, 10);
 
-    if (isNaN(hoursInput) || isNaN(minutesInput)) {
-        alarm.innerText = 'Please enter valid numbers for hours and minutes.';
+    // validate hours
+    if (!hoursInput.toString().match(numbersRegex) || hoursInput < 0 || hoursInput > 23) {
+        alarm.innerText = 'Please enter a valid number between 0 and 23 for hours.';
+        return;
+    }
+    // validate minutes
+    if (!minutesInput.toString().match(numbersRegex) || minutesInput < 0 || minutesInput > 59) {
+        alarm.innerText = 'Please enter a valid number between 0 and 59 for minutes.';
         return;
     }
 
+    alarm.innerText = `${formatTime(hoursInput)}:${formatTime(minutesInput)}`;
+
+     setAlarm(hoursInput, minutesInput);
+}
+
+function setAlarm(hoursInput, minutesInput) {
     alarmTime = new Date();
     alarmTime.setHours(hoursInput);
     alarmTime.setMinutes(minutesInput);
@@ -50,31 +70,11 @@ function setAlarm() {
     }
 }
 
-function validateAlarm() {
-    let numbersRegex = /^[0-9]+$/;
-
-    let hoursInput = parseInt(hours.value, 10);
-    let minutesInput = parseInt(minutes.value, 10);
-
-    // validate hours
-    if (!hoursInput.toString().match(numbersRegex) || hoursInput < 0 || hoursInput > 23) {
-        alarm.innerText = 'Please enter a valid number between 0 and 23 for hours.';
-        return;
-    }
-    // validate minutes
-    if (!minutesInput.toString().match(numbersRegex) || minutesInput < 0 || minutesInput > 59) {
-        alarm.innerText = 'Please enter a valid number between 0 and 59 for minutes.';
-        return;
-    }
-
-    setAlarm();
-}
-
 function clearAlarm() {
     audio.pause();
     if (alarmTimeout) {
         clearTimeout(alarmTimeout);
-        alarm.innerText = 'No alarm set';
+        alarm.innerText = '';
         displayTime.style.color = '#292C31';
     }
 }
