5c7c8dfa0e905706d60a83c902f7338b520b116f
MC Guenette <mc.c.guenette@gmail.com>
2023-11-20 13:46:57 -0600

    added clock

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,16 +1,17 @@
 'use strict';
 import {onEvent, select, selectAll } from "./utils.js";
 
-const displayTime = select('#time');
-const setAlarm = select('#set-alarm');
-const hours = select('#hours');
-const minutes = select('#minutes');
+
+// const setAlarm = select('#set-alarm');
+// const hours = select('#hours');
+// const minutes = select('#minutes');
 
 const date = new Date();
 const getHour = date.getHours();
 const getMins = date.getMinutes();
 
 function getCurrentTime() {
+    const displayTime = select('#time');
     displayTime.innerText = `${getHour}: ${getMins}`;
 }
 setInterval(getCurrentTime, 1000);
